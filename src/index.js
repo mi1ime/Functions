0f367e4627c8c4a1d8f52319a1c158f1b690e345
mi1ime <nemineko@mail.ru>
2024-12-01 16:49:59 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './css/style.css';
+
+import './js/hpStatus';
+
+// TODO: write your code in app.js
---
+++ src/js/hpStatus.js
@@ -0,0 +1,9 @@
+export default function hpStatus(player) {
+  if (player.health === 100) {
+    return 'healthy';
+  }
+  if (player.health >= 50) {
+    return 'wounded';
+  }
+  return 'critical';
+}
